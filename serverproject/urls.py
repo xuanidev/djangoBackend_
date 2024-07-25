c03d16dff9ce541cb7fe5f05d7c6d7d62cba2f5b
Juan Ignacio <juasosua@gmail.com>
2024-07-25 22:31:55 +0200

    feat: add documentation api

--- serverproject/urls.py
+++ serverproject/urls.py
@@ -1,24 +1,10 @@
-"""
-URL configuration for serverproject project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from productos import urls as productos_urls
 from clientes import urls as clientes_urls
 from ordenes import urls as ordenes_urls
+from rest_framework.documentation import include_docs_urls
 from . import views
 
 urlpatterns = [
@@ -26,6 +12,7 @@ urlpatterns = [
     path('productos/', include(productos_urls)),
     path('clientes/', include(clientes_urls)),  # Include clientes URLs
     path('ordenes/', include(ordenes_urls)),    # Include ordenes URLs
+    path('docs/', include_docs_urls(title="Store API")),
     re_path('login', views.login),
     re_path('register', views.register),
     re_path('profile', views.profile),
