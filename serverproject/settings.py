0988d8914ba762d23bc8b74e5fa593838cdd0806
Juan Ignacio <juasosua@gmail.com>
2024-07-26 20:25:26 +0200

    feat: vercel config

--- serverproject/settings.py
+++ serverproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y7-!lr5egh+l7h7pfn+xb!=^xe85%!io-jln30)!hptrugzt(8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
